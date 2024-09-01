a3fdff7b8dc72c84f13336a4563854300b9da1da
H Aditya Kumar <99540809+hadityakumar@users.noreply.github.com>
2024-09-02 00:05:27 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,27 @@ const PORT = process.env.PORT || 4000;
 
 app.use(cors());
 
+// app.get('/download', async (req, res) => {
+//     const videoURL = req.query.url;
+//     const format = req.query.format;
+
+//     if (!ytdl.validateURL(videoURL)) {
+//         return res.status(400).send('Invalid YouTube URL');
+//     }
+    
+//     let title = (format === 'mp3') ? 'audio' : 'video';
+//     res.header('Content-Disposition', `attachment; filename="${title}.${format}"`);
+
+//     const options = format === 'mp3' ? { filter: 'audioonly' } : {};
+//     ytdl(videoURL, options).pipe(res);
+
+// });
+
 app.get('/download', async (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     const videoURL = req.query.url;
     const format = req.query.format;
 
@@ -20,8 +40,8 @@ app.get('/download', async (req, res) => {
 
     const options = format === 'mp3' ? { filter: 'audioonly' } : {};
     ytdl(videoURL, options).pipe(res);
-
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
